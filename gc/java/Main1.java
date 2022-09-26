973219464ca29fcec99902cf31f43e7b6c85b6aa
zfazek <ongroa@gmail.com>
2022-09-26 20:44:24 +0200

    fix java indentations

--- gc/java/Main1.java
+++ gc/java/Main1.java
@@ -1,21 +1,20 @@
 public class Main1 {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		long start = System.currentTimeMillis();
-        long checksum = 0;
+		long checksum = 0;
 		for (int i = 0; i < 1_000_000; i++) {
 			Data data = new Data();
 			for (int j = 0; j < Data.N; j++) {
 				data.arr[j] = j;
-                checksum += data.arr[j];
+				checksum += data.arr[j];
 			}
 		}
 		long end = System.currentTimeMillis();
-        System.out.println(checksum);
+		System.out.println(checksum);
 		System.out.format("time (ms): %d\n", end - start);
 	}
-
 }
 
 class Data {
