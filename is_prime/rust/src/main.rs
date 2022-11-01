9f25e6fe6680bb364c26bd736fe936db3b74eb77
zfazek <ongroa@gmail.com>
2022-11-01 21:08:49 +0100

    enhance rust is_prime version

--- is_prime/rust/src/main.rs
+++ is_prime/rust/src/main.rs
@@ -1,17 +1,11 @@
-fn is_prime(n: i32) -> bool {
+fn is_prime(n: i64) -> bool {
     if n < 2 {
         return false;
     }
-    (2..=(n as f64).sqrt() as i32).all(|i| n % i != 0)
+    (2..=(n as f64).sqrt() as i64).all(|i| n % i != 0)
 }
 
 fn main() {
-    let mut sum: u64 = 0;
-    println!("Hello, world!");
-    for n in 1..1000000 {
-        if is_prime(n) {
-            sum += n as u64;
-        }
-    }
-    println!("{}", sum);
+    let s: i64 = (1..1000000 as i64).filter(|n| is_prime(*n)).sum();
+    println!("{}", s);
 }
